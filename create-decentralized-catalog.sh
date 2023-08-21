5fc8443b8635b947ae75a78d1cf445bdd1e3cbef
Edwin Torres <codesandtags@gmail.com>
2023-08-21 12:01:58 -0400

    feat: added scripts to generate decentralize catalog

---
+++ create-decentralized-catalog.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Define the folder containing the structure
+folder_path="./centralized"
+folder_output="./centralized"
+
+# Check if the 'apis' folder exists
+# Function to process a folder
+process_folder() {
+  local folder="$1"
+
+  # Check if the folder exists
+  if [ -d "$folder_path/$folder" ]; then
+    echo "Found '$folder' folder."
+
+    # Iterate over each file in the folder
+    for file in "$folder_path/$folder"/*; do
+      if [ -f "$file" ]; then
+        echo "File: $file"
+
+        # Extract the filename without the extension
+        filename_without_extension=$(basename -- "$file")
+        filename_without_extension="${filename_without_extension%.*}"
+
+        # Create a folder with the same name (without extension)
+        folder_name="$filename_without_extension"
+        mkdir -p "$folder_name"
+
+        # Copy the content of the file into the folder with the name 'catalog-info.yaml'
+        cp "$file" "$folder_name/catalog-info.yaml"
+
+        echo "Created folder '$folder_name' and copied content to '$folder_name/catalog-info.yaml'"
+      fi
+    done
+  else
+    echo "'$folder' folder not found."
+  fi
+}
+
+# Call the function for each folder
+process_folder "apis"
+process_folder "components"
+process_folder "systems"
+process_folder "groups"
+process_folder "domains"
---
+++ create-github-repos.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Create automatically GitHub repositories
+# Iterating over each folder in the current directory
+for folder in */; do
+  folder_name="${folder%/}"  # Remove trailing slash
+  echo "Creating GitHub repository for folder: $folder_name"
+  gh repo create "ricteam/$folder_name" --public -y
+
+  # Navigate into the folder
+  cd "$folder_name"
+
+  # Initialize a Git repository
+  git init
+  git add .
+  git commit -m "first commit"
+  git branch -M main
+
+  # Add the GitHub remote repository and push
+  git remote add origin "git@github.com:ricteam/$folder_name.git"
+  git push -u origin main
+
+  echo "Initialized Git repository, committed changes, and pushed to GitHub for folder: $folder_name"
+  
+  # Return to the parent directory
+  cd ..
+done
